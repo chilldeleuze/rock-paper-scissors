4c5388d74c1ec02ad06e576f67adc041615646d5
chilldeleuze <16662248+chilldeleuze@users.noreply.github.com>
2024-09-30 15:13:00 +0200

    add basic subfunctions

---
+++ script.js
@@ -0,0 +1,84 @@
+console.log("Hello World");
+
+
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3) + 1
+    if (num === 1) {
+        return "rock";
+    } else if (num === 2) {
+        return "paper";
+    } else if (num === 3) {
+        return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    let input = prompt("Make your choice");
+    if (input !== "rock" && input !== "paper" && input !== "scissors") {
+        input = prompt("Try again")
+    }
+    return input;
+
+}
+
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log(`Match. Both chose ${computerChoice}`);
+        return;
+    }
+
+    if (computerChoice === "rock") {
+        if (humanChoice === "paper") {
+            console.log("Human wins. Paper beats rock");
+            humanScore++;
+            return;
+        } else {
+            console.log("Computer wins. Rock beats scissors");
+            computerScore++;
+            return;
+        }
+    }
+    
+    if (computerChoice === "paper") {
+        if (humanChoice === "rock") {
+            console.log("Computer wins. Paper beats rock");
+            computerScore++;
+            return;
+        } else {
+            console.log("Human wins. Scissors beat paper");
+            humanScore++;
+            return;
+        }
+    }
+
+    if (computerChoice === "scissors") {
+        if (humanChoice === "paper") {
+            console.log("Computer wins. Scissors beats paper");
+            computerScore++;
+            return;
+        } else {
+            console.log("Human wins. Rock beats scissors");
+            humanScore++;
+            return;
+        }
+
+    }
+}
+
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    playRound(computerChoice, humanChoice);
+    console.log(`Human: ${humanScore}
+        Computer: ${computerScore}
+        `)
+    
+}
+
+playGame();
